9c62c84438392ab5eb67d52b6bf9f61e5fc07244
Angelo-Pio <angelopompeo0@gmail.com>
2026-05-20 17:11:37 +0200

    Data structures todo

--- controller/pox/ext/data_structures.py
+++ controller/pox/ext/data_structures.py
@@ -1,4 +1,6 @@
 
+import networkx as nx
+
 # For each training procedure v you need to mantain:
 # ! Global structures:
 # Graph(N,L) , node , link 
@@ -11,3 +13,18 @@
 # * Flow: workers, collector, ID , Dv , Tv = null, phase = null, stime = first time at which traffic from a worker
 # * that belongs to the flow has been detected, ftime = last time ... 
 # * Training_Procedure [ (Flows, D, T , phase, K), (Flows) ]  
+
+#! Global Variables
+
+# Graph
+# Current_procedure_id
+# TrainingProcedures = []
+# Capacity
+
+
+
+# TODO:
+# 1. Finalizing graph, node and link data structures
+# 2. Populating statically the grah with netw topology and collectors
+# 3. Undestrand how to discover workers and populate Flow.workers for a Flow 
+# 4. Undestainding how to update link residual_capacity(how much data coming from a worker etc.)
